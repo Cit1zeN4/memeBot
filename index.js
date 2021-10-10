ec80f25cc526667dc20014c742c6f38f0e5c92ad
Max <maximrimkus@yandex.by>
2021-10-10 17:21:24 +0300

    dev test

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const { Telegraf } = require("telegraf");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.telegram.getMe().then((botInfo) => {
+  bot.options.username = botInfo.username;
+  console.log(botInfo);
+});
+
 bot.start((ctx) => ctx.reply("hello bro"));
+
+bot.command("gg", (ctx) => ctx.reply("gg"));
 bot.launch().then(() => console.log("Bot started"));
